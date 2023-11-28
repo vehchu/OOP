a1995de14eadb76506fa3d96399bf9571eed0345
vehchu <victoriennetiu@gmail.com>
2023-11-28 09:12:45 +0800

    my oop project

---
+++ booty/booty/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace booty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult bruh()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult Register(FormCollection fc)
+        {
+            String fname = Convert.ToString(fc["Firstname"]);
+            String lname = Convert.ToString(fc["Lastname"]);
+            String email = Convert.ToString(fc["Email"]);
+            String password = Convert.ToString(fc["Password"]);
+
+            FriendsEntities fe = new FriendsEntities();
+            User u = new User();
+            u.FirstName = fname;
+            u.LastName = lname;
+            u.Email = email;
+            u.Password = password;
+            u.RoleID = 3;
+
+            fe.Users.Add(u);
+            fe.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult UserUpdate()
+        {
+          
+            FriendsEntities rdbe = new FriendsEntities();
+            User u = (from a in rdbe.Users
+                     where a.UserID == 3
+                     select a).FirstOrDefault();
+
+            u.Password = "guapo";
+            u.RoleID = 2;
+            rdbe.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult UserDelete()
+        {
+
+            FriendsEntities rdbe = new FriendsEntities();
+            User u = (from a in rdbe.Users
+                      where a.UserID == 3
+                      select a).FirstOrDefault();
+            rdbe.Users.Remove(u);
+            rdbe.SaveChanges();
+
+            return View();
+        }
+    }
+}
